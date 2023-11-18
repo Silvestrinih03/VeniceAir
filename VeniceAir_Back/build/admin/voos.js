3f939a6c38e2b7dd5439d887eaa2f55711e77340
Nicole Silvestrini <nsgarrio@gmail.com>
2023-11-18 12:52:08 -0300

    Back inserir voo funcionando

--- VeniceAir_Back/build/admin/voos.js
+++ VeniceAir_Back/build/admin/voos.js
@@ -51,15 +51,58 @@ exports.vooRouter.get("/listarVoos", (req, res) => __awaiter(void 0, void 0, voi
         res.send(cr);
     }
 }));
+// // Inserir voos
+// vooRouter.put("/inserirVoo", async(req,res)=>{
+//   const trecho = req.body.trecho as number;
+//   const data_partida = req.body.data_partida as Date;
+//   const hora_partida = req.body.hora_partida as string;
+//   const hora_chegada = req.body.hora_chegada as string;
+//   const aeroporto_partida = req.body.aeroporto_partida as number;
+//   const aeroporto_chegada = req.body.aeroporto_chegada as number;
+//   const valor = req.body.valor as number;
+//   let cr: CustomResponse = {
+//     status: "ERROR",
+//     message: "",
+//     payload: undefined,
+//   };
+//   let conn;
+//   try{
+//     conn = await oracledb.getConnection({
+//        user: process.env.ORACLE_DB_USER,
+//        password: process.env.ORACLE_DB_SECRET,
+//        connectionString: process.env.ORACLE_DB_CONN_STR,
+//     });
+//     const cmdInsertVoo = 'INSERT INTO VOOS (TRECHO, DATA_PARTIDA, HORA_PARTIDA, HORA_CHEGADA, AEROPORTO_PARTIDA, AEROPORTO_CHEGADA, VALOR) VALUES (:1, :2, :3, :4, :5, :6, :7)';
+//     const dados = [trecho, data_partida, hora_partida, hora_chegada, aeroporto_partida, aeroporto_chegada, valor];
+//     let resInsert = await conn.execute(cmdInsertVoo, dados);
+//     await conn.commit();
+//     const rowsInserted = resInsert.rowsAffected
+//     if(rowsInserted !== undefined &&  rowsInserted === 1) {
+//       cr.status = "SUCCESS"; 
+//       cr.message = "Voo inserido.";
+//     }
+//   }catch(e){
+//     if(e instanceof Error){
+//       cr.message = e.message;
+//       console.log(e.message);
+//     }else{
+//       cr.message = "Erro ao conectar ao oracle. Sem detalhes";
+//     }
+//   } finally {
+//     if(conn!== undefined){
+//       await conn.close();
+//     }
+//     res.send(cr);  
+//   }
+// });
 exports.vooRouter.put("/inserirVoo", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    // meu deus era só colocar letra maiuscula
-    const trecho = req.body.Trecho;
-    const data_partida = req.body.Data;
-    const hora_partida = req.body.HorarioPartida;
-    const hora_chegada = req.body.HorarioChegada;
-    const aeroporto_partida = req.body.AeroportoPartida;
-    const aeroporto_chegada = req.body.AeroportoChegada;
-    const valor = req.body.Valor;
+    const trecho = req.body.trecho;
+    const data_partida = new Date(req.body.data_partida);
+    const hora_partida = req.body.hora_partida;
+    const hora_chegada = req.body.hora_chegada;
+    const aeroporto_partida = req.body.aeroporto_partida;
+    const aeroporto_chegada = req.body.aeroporto_chegada;
+    const valor = req.body.valor;
     let cr = {
         status: "ERROR",
         message: "",
@@ -72,16 +115,7 @@ exports.vooRouter.put("/inserirVoo", (req, res) => __awaiter(void 0, void 0, voi
             password: process.env.ORACLE_DB_SECRET,
             connectionString: process.env.ORACLE_DB_CONN_STR,
         });
-        console.log("Dados para inserção:", {
-            trecho,
-            data_partida,
-            hora_partida,
-            hora_chegada,
-            aeroporto_partida,
-            aeroporto_chegada,
-            valor,
-        });
-        const cmdInsertVoo = `INSERT INTO VOOS VALUES (SEQ_TRECHOS.NEXTVAL, :1, :2, :3, :4, :5, :6, :7)`;
+        const cmdInsertVoo = 'INSERT INTO VOOS (TRECHO, DATA_PARTIDA, HORA_PARTIDA, HORA_CHEGADA, AEROPORTO_PARTIDA, AEROPORTO_CHEGADA, VALOR) VALUES (:1, :2, :3, :4, :5, :6, :7)';
         const dados = [trecho, data_partida, hora_partida, hora_chegada, aeroporto_partida, aeroporto_chegada, valor];
         let resInsert = yield conn.execute(cmdInsertVoo, dados);
         yield conn.commit();
@@ -97,7 +131,7 @@ exports.vooRouter.put("/inserirVoo", (req, res) => __awaiter(void 0, void 0, voi
             console.log(e.message);
         }
         else {
-            cr.message = "Erro ao conectar ao oracle. Sem detalhes";
+            cr.message = "Erro ao conectar ao Oracle. Sem detalhes.";
         }
     }
     finally {
--- VeniceAir_Back/src/admin/voos.ts
+++ VeniceAir_Back/src/admin/voos.ts
@@ -50,19 +50,17 @@ vooRouter.get("/listarVoos", async(req,res)=>{
 
 });
 
-vooRouter.put("/inserirVoo", async(req,res)=>{
-  
-  // meu deus era só colocar letra maiuscula
-  const trecho = req.body.Trecho as number;
-  const data_partida = req.body.Data as string;
-  const hora_partida = req.body.HorarioPartida as string;
-  const hora_chegada = req.body.HorarioChegada as string;
-  const aeroporto_partida = req.body.AeroportoPartida as number;
-  const aeroporto_chegada = req.body.AeroportoChegada as number;
-  const valor = req.body.Valor as number;
+// Função OK
+vooRouter.put("/inserirVoo", async (req, res) => {
+  const trecho = req.body.trecho as number;
+  const data_partida = new Date(req.body.data_partida);
+  const hora_partida = req.body.hora_partida as string;
+  const hora_chegada = req.body.hora_chegada as string;
+  const aeroporto_partida = req.body.aeroporto_partida as number;
+  const aeroporto_chegada = req.body.aeroporto_chegada as number;
+  const valor = req.body.valor as number;
 
-
-  let cr: CustomResponse = {
+  let cr = {
     status: "ERROR",
     message: "",
     payload: undefined,
@@ -70,52 +68,43 @@ vooRouter.put("/inserirVoo", async(req,res)=>{
 
   let conn;
 
-  try{
+  try {
     conn = await oracledb.getConnection({
       user: process.env.ORACLE_DB_USER,
       password: process.env.ORACLE_DB_SECRET,
       connectionString: process.env.ORACLE_DB_CONN_STR,
     });
 
-    console.log("Dados para inserção:", {
-      trecho,
-      data_partida,
-      hora_partida,
-      hora_chegada,
-      aeroporto_partida,
-      aeroporto_chegada,
-      valor,
-    });
-
-
-    const cmdInsertVoo = `INSERT INTO VOOS VALUES (SEQ_TRECHOS.NEXTVAL, :1, :2, :3, :4, :5, :6, :7)`
+    const cmdInsertVoo =
+      'INSERT INTO VOOS (TRECHO, DATA_PARTIDA, HORA_PARTIDA, HORA_CHEGADA, AEROPORTO_PARTIDA, AEROPORTO_CHEGADA, VALOR) VALUES (:1, :2, :3, :4, :5, :6, :7)';
 
     const dados = [trecho, data_partida, hora_partida, hora_chegada, aeroporto_partida, aeroporto_chegada, valor];
     let resInsert = await conn.execute(cmdInsertVoo, dados);
     await conn.commit();
-  
-    const rowsInserted = resInsert.rowsAffected
-    if(rowsInserted !== undefined &&  rowsInserted === 1) {
-      cr.status = "SUCCESS"; 
+
+    const rowsInserted = resInsert.rowsAffected;
+    if (rowsInserted !== undefined && rowsInserted === 1) {
+      cr.status = "SUCCESS";
       cr.message = "Voo inserido.";
     }
-
-  }catch(e){
-    if(e instanceof Error){
+  } catch (e) {
+    if (e instanceof Error) {
       cr.message = e.message;
       console.log(e.message);
-    }else{
-      cr.message = "Erro ao conectar ao oracle. Sem detalhes";
+    } else {
+      cr.message = "Erro ao conectar ao Oracle. Sem detalhes.";
     }
   } finally {
-    if(conn!== undefined){
+    if (conn !== undefined) {
       await conn.close();
     }
-    res.send(cr);  
+    res.send(cr);
   }
 });
 
-// Função OK
+
+
+// Função ???
 vooRouter.delete("/excluirVoo/:codigo", async (req, res) => {
   const codigo = req.params.codigo;
 
